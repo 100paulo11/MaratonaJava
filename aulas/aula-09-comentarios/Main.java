8c7a157528477d1c9b439ee33fe6bb36c228b660
Paulocode110 <161464564+Paulocode110@users.noreply.github.com>
2026-09-09 19:03:20 -0300

    Adiciona documentação e exemplo da Aula 09

---
+++ aulas/aula-09-comentarios/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Comentário de uma linha
+        System.out.println("Olá, Java!");
+
+        /*
+         * Comentário de múltiplas linhas.
+         * Este texto não será executado.
+         */
+
+        System.out.println("Aprendendo comentários em Java!");
+
+        /**
+         * Este também é um comentário.
+         * É utilizado para documentação (Javadoc).
+         */
+        System.out.println("Fim da aula 09!");
+    }
+}
